31909eb3d43812d4bc68271a653f9600d17ced69
Adriano Frota <adriano.frota.silva@accenture.com>
2023-02-19 18:44:23 -0300

    MapPost to ask chatgtp integrated v1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-using System.Security.AccessControl;
 using System.Text;
-using Newtonsoft.Json;
+using Newtonsoft.Json; 
+using Newtonsoft.Json.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
@@ -36,9 +36,54 @@ app.MapGet("/questionsQS", (int query_string_qtd) =>  {
 
 app.MapPost("/questions", (Question question) => question); 
 
+//TODO: put your key here (https://platform.openai.com/account/api-keys) 
+var apiKey = "your key here";
+
+app.MapPost("/questionChatGPT", async (Question question) => {
+    if(string.IsNullOrEmpty(question.Asked)){
+      return await ChatGPT.GetOpenAICompletion("What is the meaning of life?", apiKey); 
+    }
+    else {
+      return await ChatGPT.GetOpenAICompletion( question.Asked, apiKey);      
+    }
+});
 
 app.Run();
 
 record Question(int Id, string Asked, string Answer = "")
 { 
 }
+
+public static class ChatGPT{
+
+    public static async Task<string> GetOpenAICompletion(string prompt, string apiKey)
+    {
+        using var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+        var json = new JObject(
+            new JProperty("model", "text-davinci-002"),
+            new JProperty("prompt", prompt),
+            new JProperty("temperature", 0.5),
+            new JProperty("max_tokens", 50),
+            new JProperty("n", 1),
+            new JProperty("stop", ".")
+        );
+
+        using var content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+        using var response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            return $"Request failed with status code {response.StatusCode}";
+        }
+
+        var responseJson = JObject.Parse(await response.Content.ReadAsStringAsync());
+        var choices = responseJson.SelectToken("choices");
+        var completion = choices?[0].SelectToken("text")?.ToString();
+
+        return completion;
+    }
+
+}
+
