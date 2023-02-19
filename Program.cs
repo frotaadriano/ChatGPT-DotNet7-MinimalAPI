f30aac7bd854f0a6f7f8aaf9103ff34dfdfc2cb3
Adriano Frota <adriano.frota.silva@accenture.com>
2023-02-19 16:13:58 -0300

    clean code and first GET

--- Program.cs
+++ Program.cs
@@ -1,24 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Text;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    
-}
-
 app.UseHttpsRedirection(); 
- 
-// .WithName("GetWeatherForecast")
-// .WithOpenApi();
+
+app.MapGet("/questions", () => new Question[]{ new (1, "answer1"), new (2, "answer2")});
 
 app.Run();
- 
+
+record Question(int Id, String Answer);
